185e4b423de151485113bc4c6380860c19def6a1
trendsandtactics <trendsntactics@gmail.com>
2025-10-07 15:14:54 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Footer from "~/components/Section/Common/Footer";
-import Header from "~/components/Section/Common/Header";
+import HeaderFour from "~/components/Section/Common/HeaderFour";
 import About from "~/components/Section/Home-1/About";
 import Blog from "~/components/Section/Home-1/Blog";
 import ChooseUs from "~/components/Section/Home-1/ChooseUs/ChooseUs";
